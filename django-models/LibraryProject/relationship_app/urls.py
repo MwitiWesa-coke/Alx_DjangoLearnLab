557884a8098edac56d7fa81699ae35fe2e61c355
MwitiWesa-coke <mwitiwesa4@gmail.com>
2025-08-03 15:38:39 +0300

    Add user authentication views, URLs, and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from .views import list_books
 
 urlpatterns = [
     # Book Actions (Custom Permissions)
